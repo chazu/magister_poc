80935368ce779af642a20f2ee0d0b9f2204f1d38
Chazu <chaz@symbolforce.com>
2015-05-20 12:50:26 -0400

    get scheduler working, add to magister.rb

--- lib/scheduler.rb
+++ lib/scheduler.rb
@@ -2,9 +2,11 @@ require 'singleton'
 require 'rest-client'
 
 module Magister
+
   class Scheduler
     include Singleton
     include Helpers
+
     def initialize
       puts "Loading scheduler contents"
       scheduler_entity = Entity.find("/_scheduler")
@@ -17,7 +19,7 @@ module Magister
                     is_context: true}, nil).persist_recursively
       end
 
-      scheduler = Rufus::Scheduler.new
+      @scheduler = Rufus::Scheduler.new
 
       if scheduler_contents
         scheduler_contents.each do |index_key|
@@ -25,19 +27,28 @@ module Magister
           parsed = parse_schedule_item(ent.data)
           
           puts "Processing scheduler item: " + parsed.to_s
-          
-          scheduler.every map_schedule_item_frequency(parsed) do
 
-            RestClient.send(parsed["method"].downcase.to_sym,                    "#{Config.options["host"]}:#{Config.options["port"]}#{parsed["index_key"]}", {})            
-
+          # Set up the repeating jobs
+          @scheduler.every map_schedule_item_frequency(parsed) do
+            index_key = ent.index_key
+            puts "Running schedule item " + index_key
+            RestClient.send(parsed["method"].downcase.to_sym,
+                            "#{Config.options["host"]}:#{Config.options["port"]}#{parsed["index_key"]}",
+                            {})            
           end
         end
       end
 
-      scheduler.every '30s' do
+      @scheduler.every '30s' do
         Magister.sync_index_to_store
       end
     end
+
+    #################
+    # Helpers
+    #################
+    
+    # map a schedule entry to a string which can be fed into the scheduler instance
     def map_schedule_item_frequency parsed_item
       parsed_item["number"].to_s + map_schedule_item_unit(parsed_item["unit"])
     end
@@ -56,7 +67,7 @@ module Magister
         'h' # Default to hours
       end
     end
-    
+
     def parse_schedule_item entity_data
       parse_schedule_item_from_json entity_data
     end
